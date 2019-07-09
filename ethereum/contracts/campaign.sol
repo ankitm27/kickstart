bda7f46cbfedbb7c6053a127a17d7440a1b3da99
ankitm27 <anki.malhotra00@gmail.com>
2019-07-09 13:21:51 +0530

    code changes

--- ethereum/contracts/campaign.sol
+++ ethereum/contracts/campaign.sol
@@ -1,75 +1,96 @@
 pragma solidity ^0.4.17;
 
-contract CampaignFactory{
+contract CampaignFactory {
     address[] public deployedCampaigns;
-    function createCampaign(uint minimum) public{
-        address newCampaign = new Campaign(minimum,msg.sender);
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
-    
-    function getDeployedCampaign() public view returns (address[]){
+
+    function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     }
 }
-contract Campaign{
-    address public manager;
-    uint public minimumContribution;
+
+contract Campaign {
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
-        uint approvalCounts;
-        mapping(address => bool) approvals;   
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
+
     Request[] public requests;
-    mapping(address => bool) public approvals;
-    uint public approvalCounts;
-    
-    modifier restricted (){
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function Campaign(uint minimum,address creator) public{
+
+    function Campaign(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
-    
-    function contribute() public payable{
+
+    function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvals[msg.sender] = true;
-        approvalCounts ++;
-        
+
+        approvers[msg.sender] = true;
+        approversCount++;
     }
-    
-    function createRequest(string description, uint value,address recipient) public restricted{
-        require(approvals[msg.sender]);
+
+    function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-            description:description,
-            value:value,
-            recipient:recipient,
-            complete:false,
-            approvalCounts:0
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
         });
+
         requests.push(newRequest);
     }
-    
+
     function approveRequest(uint index) public {
-        require(approvals[msg.sender]);
         Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
-        request.approvalCounts ++;
+
         request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
-    
-    function finalizeRequest(uint index) public restricted{
+
+    function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
-        require(request.approvalCounts > (approvalCounts/2));
+
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
+
         request.recipient.transfer(request.value);
         request.complete = true;
-        
     }
-    
+
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          this.balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
 }
---
+++ ethereum/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'nerve fly squeeze fault shadow alcohol crunch economy come point feel fox',
+  'https://ropsten.infura.io/v3/22c974a7a4c1423394ebfba3fd90d19d'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
